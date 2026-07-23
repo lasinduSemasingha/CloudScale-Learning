99f5114fe99f206e97962e971fbfeb9b90013121
lasindusemasingha <lasindusemasingha@gmail.com>
2026-07-23 10:57:23 +0530

    Add project files.

---
+++ CloudScale.WebAPI/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿namespace CloudScale.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("test")]
+        public IActionResult Get()
+        {
+            return Ok("Healthy");
+        }
+    }
+}
